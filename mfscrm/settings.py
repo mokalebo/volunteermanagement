aecddb295e9e0e9a85255b0d8c60b0e2bbf31b4b
Matthew Okalebo <mokalebo@unomaha.edu>
2019-12-11 17:30:50 -0600

    requirements.txt and settings.py modified and ready for PROD

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'test_utils',
     'crm',
     'django_extensions',
     'crispy_forms',
